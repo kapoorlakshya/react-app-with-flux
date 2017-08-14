81d2c219b281335898557ab3895d795aff0d2937
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-08-13 23:14:03 -0700

    Add 404 page route

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRouter = Router.NotFoundRoute;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
       <DefaultRoute handler={require('./components/homePage')} />
       <Route name="authors" handler={require('./components/authors/authorPage')}/>
       <Route name="about" handler={require('./components/about/aboutPage')} />
+      <NotFoundRouter handler={require('./components/notFoundPage')} />
     </Route>
 );
 

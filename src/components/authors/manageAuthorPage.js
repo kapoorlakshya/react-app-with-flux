83880abcc678b603a7c83d10cb3ff5eb841c2d89
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-08-15 00:47:27 -0700

    Add saving author feature

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,11 +1,41 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+  mixins: [
+    Router.Navigation
+  ],
+
+  // Definite initial state for author object
+  getInitialState: function() {
+    return {
+      author: {id: '', firstName: '', lastName: ''}
+    };
+  },
+
+  setAuthorState: function(event) {
+    var field = event.target.name;
+    var value = event.target.value;
+    this.state.author[field] = value;
+    return this.setState({author: this.state.author});
+  },
+
+  saveAuthor: function (event) {
+    event.preventDefault();
+    AuthorApi.saveAuthor(this.state.author);
+    this.transitionTo('authors');
+  },
+
   render: function () {
     return (
-      <h1>Manage Author</h1>
+      <AuthorForm
+      author={this.state.author}
+      onChange={this.setAuthorState}
+      onSave={this.saveAuthor} />
     );
   }
 });

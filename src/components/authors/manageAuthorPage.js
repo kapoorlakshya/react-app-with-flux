4a7f13b0176145749f1daa434c6078775bb20e8a
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-08-15 01:24:36 -0700

    Prompt user when leaving unsaved data

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -10,16 +10,26 @@ var ManageAuthorPage = React.createClass({
   mixins: [
     Router.Navigation
   ],
+  
+  statics: {
+    willTransitionFrom: function (transition, component) {
+      if (component.state.dirty && !confirm('Leave without saving?')) {
+        transition.abort();
+      }
+    }
+  },
 
   // Definite initial state for author object
   getInitialState: function() {
     return {
       author: {id: '', firstName: '', lastName: ''},
-      errors: {}
+      errors: {},
+      dirty: false
     };
   },
 
   setAuthorState: function(event) {
+    this.setState({dirty: true});
     var field = event.target.name;
     var value = event.target.value;
     this.state.author[field] = value;
@@ -51,6 +61,7 @@ var ManageAuthorPage = React.createClass({
       return;
     }
     AuthorApi.saveAuthor(this.state.author);
+    this.setState({dirty: false});
     toastr.success('Author saved!');
     this.transitionTo('authors');
   },

1ba3b0660f6d6d2beae6e7b0c65f066048b09cb0
Lakshya Kapoor <kapoorlakshya@gmail.com>
2017-08-28 01:22:27 -0700

    Add store listeners and delete author functionality

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,5 +4,7 @@ var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
   INITAILIZE: null,
-  CREATE_AUTHOR: null
+  CREATE_AUTHOR: null,
+  UPDATE_AUTHOR: null,
+  DELETE_AUTHOR: null
 });
